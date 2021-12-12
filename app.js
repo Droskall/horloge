ef46d5b41a86eb18648bd2ca92ad734fcf835d72
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-12 18:12:18 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// Actual Date:
+let dayDate = Date();
+let dateAffichage = document.getElementById('date');
+let actualDate = new Date();
+let actualDateMouth = actualDate.getMonth();
+let actualDateDay = actualDate.getDate();
+let actualDateYear = actualDate.getFullYear();
+
+dateAffichage.innerHTML = actualDateDay + " / " + (actualDateMouth + 1) + " / " + actualDateYear;
+dateAffichage.style.margin = "0";
+
+// Clock:
+function clock (){
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds();
+
+    let timeOut = setTimeout(function (){
+
+        let time = [
+            {
+                time: 'hours',
+                angle: (hours * 30) + (minutes / 2)
+            },
+            {
+                time: 'minutes',
+                angle: (minutes * 6)
+            },
+            {
+                time: 'seconds',
+                angle: (seconds * 6)
+            }
+        ];
+
+        for (let i = 0; i < time.length; i++) {
+            let elements = document.querySelectorAll('#' + time[i].time);
+            for (let j = 0; j < elements.length; j++) {
+                elements[j].style.transform = 'rotateZ('+ time[i].angle + 'deg)';
+                if (time[i].time === 'minutes') {
+                    elements[j].parentNode.setAttribute('data-second-angle', time[i + 1].angle)
+                }
+            }
+        }
+        clock();
+    }, 1000);
+
+}
+
+clock();
